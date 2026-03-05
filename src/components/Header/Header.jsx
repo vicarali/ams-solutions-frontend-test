43581d7a35f6995ca02049935c714d34ddc1d47e
Vicarali <vicarali@gmail.com>
2026-03-05 19:11:51 +0100

    feat: implement navigational menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,28 @@
 import viteLogo from "/vite.svg";
 import cartIcon from "/cart-icon.png";
 import styles from "./Header.module.css";
-import { Link } from "react-router";
+import { Link, NavLink, useLocation } from "react-router";
 import { useContext } from "react";
 import { CartContext } from "../../contexts/CartContext/CartContext";
 
 export function Header() {
 	const { cartItemsCount } = useContext(CartContext);
+	const { pathname: currentPathname } = useLocation();
 
 	return (
 		<header className={styles.header}>
-			<Link to="/">
-				<img src={viteLogo} alt="Vite logo" className={styles.icon} />
-			</Link>
+			<div className={styles.linksContainer}>
+				<Link to="/">
+					<img src={viteLogo} alt="Vite logo" className={styles.icon} />
+				</Link>
+				<NavLink to="/">Home</NavLink>
+				{currentPathname !== "/" && (
+					<>
+						<span>{">"}</span>
+						<NavLink to={currentPathname}>Product details</NavLink>
+					</>
+				)}
+			</div>
 			<div className={styles.cartContainer}>
 				<img
 					src={cartIcon}
